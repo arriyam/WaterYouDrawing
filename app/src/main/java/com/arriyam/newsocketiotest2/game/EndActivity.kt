e9f5aa15e2273c83b6656d4e40190bf4739c50db
Arriyam <thushen@arriyam.com>
2021-05-14 12:04:22 -0400

    More stuff added to the EndActivity.

--- app/src/main/java/com/arriyam/newsocketiotest2/game/EndActivity.kt
+++ app/src/main/java/com/arriyam/newsocketiotest2/game/EndActivity.kt
@@ -7,7 +7,6 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.arriyam.newsocketiotest2.MainActivity
 import com.arriyam.newsocketiotest2.R
-import com.arriyam.newsocketiotest2.login.UsernameActivity
 import com.arriyam.newsocketiotest2.socket.SocketHandler
 
 
@@ -15,27 +14,56 @@ class EndActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_end)
+//        setContentView(R.layout.activity_end_blue)
 
-        //      Socket connection for this activity
-        val nSocket= SocketHandler.getSocket()
-
-        val textViewTitle=findViewById<TextView>(R.id.textViewTitle)
-        val btnReturnMainMenu=findViewById<Button>(R.id.btnReturnMainMenu)
-        val textViewScore=findViewById<TextView>(R.id.textViewScore)
 
         val pointsViewDisplay=intent.getStringExtra("Points_Data")
         val roundTotalWinner=intent.getStringExtra("Total_Winner_Data")
 
 
-//         Removed because there was a big on the server. Didn't display proper point values.
-//        textViewScore.text="Final "+pointsViewDisplay
 
         when (roundTotalWinner){
-            "R"->textViewTitle.text="Team Red wins!"
-            "B"->textViewTitle.text="Team Blue wins!"
-            else ->textViewTitle.text="It is a Tie"
+            "R"->{
+                setContentView(R.layout.activity_end_red)
+
+            }
+            "B"->{
+                setContentView(R.layout.activity_end_blue)
+
+            }
+            else ->{
+                setContentView(R.layout.activity_end)
+
+            }
         }
+
+        val textViewTitle=findViewById<TextView>(R.id.textViewTitle)
+        val btnReturnMainMenu=findViewById<Button>(R.id.btnReturnMainMenu)
+        val textViewScore=findViewById<TextView>(R.id.textViewScore)
+
+
+        when (roundTotalWinner){
+            "R"->{
+
+                textViewTitle.text="Team Red wins!"
+            }
+            "B"->{
+
+                textViewTitle.text="Team Blue wins!"
+            }
+            else ->{
+
+                textViewTitle.text="It is a Tie"
+            }
+        }
+
+        //         Removed because there was a big on the server. Didn't display proper point values.
+        textViewScore.text="Final "+pointsViewDisplay
+
+
+
+        //      Socket connection for this activity
+        val nSocket= SocketHandler.getSocket()
 
         btnReturnMainMenu.setOnClickListener{
             val intent= Intent(this, MainActivity::class.java)
